84f7cfeb27c35a6712ad45282f28505e272c892f
SamCHogg <sam.hogg@arm.com>
2024-05-01 18:11:09 +0100

    Day 1 Part 1

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use std::fs;
+use std::io::{BufRead, BufReader, Error};
+use std::string::String;
+
+fn process_line(line: String) -> u32 {
+    let mut first = 0;
+    for c in line.chars() {
+        if c.is_numeric() {
+            first = c.to_digit(10).unwrap();
+            break;
+        }
+    }
+
+    let mut last = 0;
+    for c in line.chars().rev() {
+        if c.is_numeric() {
+            last = c.to_digit(10).unwrap();
+            break;
+        }
+    }
+
+    (first * 10) + last
+}
+
+fn main() -> Result<(), Error> {
+    let mut total = 0;
+    let buff_reader = BufReader::new(fs::File::open("./input.txt")?);
+    for line in buff_reader.lines() {
+        total += process_line(line?);
+    }
+    println!("{}", total);
+    Ok(())
 }
